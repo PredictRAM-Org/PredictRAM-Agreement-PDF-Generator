e38abd109b67ea8e46e1c1e64226867c0bf39b04
PredictRAM DataHub <95133921+PredictRAM-Org@users.noreply.github.com>
2023-12-28 21:39:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,10 @@ from io import BytesIO
 def generate_agreement(name, email, contact_number, date):
     pdf = FPDF()
     pdf.add_page()
+
+    # Add company logo at the top of the page
+    pdf.image("path/to/company_logo.png", x=10, y=10, w=30)
+
     pdf.set_font("Arial", size=12)
 
     # Add content to the PDF
@@ -21,6 +25,47 @@ def generate_agreement(name, email, contact_number, date):
                                f"Also, please sign the duplicate of this offer as your acceptance and forward the same to us.\n\n"
                                f"Congratulations!\n"
                                f"Team PredictRAM", align='L')
+
+    # Add Annexure A
+    pdf.ln(10)
+    pdf.cell(200, 10, txt="Training & Training & Internship Agreement", ln=True, align='C')
+    pdf.ln(10)
+    pdf.cell(200, 10, txt="This Training & Training & Internship Agreement (this “Agreement”) is hereby entered into Day.", ln=True, align='L')
+    pdf.cell(200, 10, txt="(“Effective Date”) - between the Intern and Company noted below, with respect to the Services and Project defined herein below.", ln=True, align='L')
+    pdf.ln(10)
+    pdf.cell(200, 10, txt="Intern Name:", ln=True, align='L')
+    pdf.cell(200, 10, txt=f"{name}", ln=True, align='L')
+    pdf.cell(200, 10, txt="Intern Email:", ln=True, align='L')
+    pdf.cell(200, 10, txt=f"{email}", ln=True, align='L')
+    pdf.cell(200, 10, txt="Contact Number:", ln=True, align='L')
+    pdf.cell(200, 10, txt=f"{contact_number}", ln=True, align='L')
+    pdf.cell(200, 10, txt="Intern Location:", ln=True, align='L')
+    pdf.ln(10)
+    pdf.cell(200, 10, txt="Company Representative Name:", ln=True, align='L')
+    pdf.cell(200, 10, txt="(“SUBIR SINGH”):", ln=True, align='L')
+    pdf.cell(200, 10, txt="Business Name: PredictRAM – Params Data Provider Pvt Ltd & Associate Companies.", ln=True, align='L')
+    pdf.cell(200, 10, txt="Tel: 9873387612", ln=True, align='L')
+    pdf.cell(200, 10, txt="Email: subir@predictram.com  i.subirsingh@gmail.com", ln=True, align='L')
+    pdf.ln(10)
+    pdf.cell(200, 10, txt="Terms & Conditions", ln=True, align='L')
+    pdf.ln(10)
+    pdf.multi_cell(0, 10, txt="BACKGROUND: The Company has developed a comprehensive 6-month training and internship program focused on Risk and Asset Management (hereinafter referred to as the \"Program\"). The Program offers valuable training and practical experience to interns to prepare them for a career in the financial industry. It is important to note that the cost for joining this program is free. Upon successful completion of the Program, the Intern shall have the opportunity to work with the Company, associate company or partnered company in managing real client portfolios.", align='L')
+    pdf.ln(10)
+    pdf.multi_cell(0, 10, txt="DURATION OF TRAINING & INTERNSHIP: The duration of training and internship program with the Company is for a period of six (6) months, commencing on the date that the Intern signs or accept this Agreement (the \"Start Date\") mentioned above.", align='L')
+    pdf.ln(10)
+    pdf.cell(200, 10, txt="PROGRAM DETAILS:", ln=True, align='L')
+    pdf.ln(10)
+    pdf.multi_cell(0, 10, txt="1. NISM Research Analyst Exam Preparation: The Intern will receive recorded sessions and study materials to prepare for the NISM Research Analyst Exam.\n"
+                               "2. Data-Driven Client Interaction: The Program will develop the Intern's skills in data-driven client engagement, utilizing analytics to assist clients in achieving their financial goals.\n"
+                               "3. Alternative Data & Sentiment Analysis: The Intern will learn to analyze non-traditional data sources to assess a company's future performance by using economic data and other alternative parameters.\n"
+                               "4. Company Management Conference Participation: The Program will familiarize the Intern with attending company management conferences, enabling them to ask insightful questions to top management.\n"
+                               "5. Diverse Asset Class Management: The Intern will gain expertise in managing various asset classes, including traditional equities, modern digital assets and others.\n"
+                               "6. Data Analytics Training: The Intern will receive training in data analytics using Python Basics.\n"
+                               "7. Real Client Portfolio Management: Following successful clearance of NISM exams, the Intern will have the opportunity to manage real client portfolios, gaining hands-on experience.\n"
+                               "8. ESG Analysis with Alternative Data: The Program will provide training on integrating ESG analysis into portfolio management using alternative data sources.\n"
+                               "9. Regulatory Compliance: The Intern will receive training on the Code of Conduct for Financial Professionals, ethical standards, regulations, and practical scenarios.", align='L')
+    pdf.ln(10)
+    pdf.multi_cell(0, 10, txt="COMPENSATION: Upon successful completion of the Program, the Company agrees to offer the Intern a starting monthly salary of INR 10,000 to manage real client portfolios. The terms and conditions of the Intern's employment with the Company, including but not limited to working hours, benefits, confidentiality, and termination procedures, shall be outlined in a separate employment agreement to be provided to the Intern upon successfully completion of the Program.", align='L')
 
     return pdf.output(dest='S').encode('latin1')
 
