0cf3c454f2e03808128bf52e24a287de234a51a8
PredictRAM DataHub <95133921+PredictRAM-Org@users.noreply.github.com>
2023-12-28 22:20:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,11 +8,11 @@ def generate_agreement(name, email, contact_number, date):
     pdf.set_font("Arial", size=12)
 
     # Add company logo at the top of the page
-    pdf.image("Predictram_logo.png", x=10, y=10, w=30)
+    pdf.image("path/to/company_logo.png", x=10, y=10, w=30)
 
     # Add content to the PDF
     # Adjust Y-coordinate to align date with the logo
-    pdf.cell(200, 10, txt=f"Date: {date}", ln=True, align='L', ln=12) 
+    pdf.cell(200, 10, txt=f"Date: {date}", ln=True, align='L') 
 
     pdf.multi_cell(0, 10, txt=f"Dear {name},\n\n"
                                f"I am delighted & excited to welcome you to PredictRAM for Risk & Asset Management Training cum "
